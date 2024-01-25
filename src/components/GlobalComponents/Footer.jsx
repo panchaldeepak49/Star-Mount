576401ec4af9d27697d08321155dd6aaea3dd1b6
Deepak <panchaldeepak49@gmail.com>
2024-01-25 20:06:22 +0530

    footer again code push

--- src/components/GlobalComponents/Footer.jsx
+++ src/components/GlobalComponents/Footer.jsx
@@ -18,10 +18,10 @@ const Footer = () => {
  {/* footer part-1 */}
  <div className='ml-2 sm:ml-10'>
      <img src={""} className='w-[15%]'></img>
-     <p className='text-white sm:text-sm text-[0.5rem] sm:mt-5 mt-3'>Star Mount Technology</p>
-     <p className='text-white sm:text-sm text-[0.5rem]'>Plot No.304 Kh. No.-1074,Kapashera</p>
-     <p className='text-white sm:text-sm text-[0.5rem]'>New Delhi-110037 India </p>
-     <div className='flex gap-5 sm:mt-5 mt-3'>
+     <p className='text-white sm:text-sm text-[0.3rem] sm:mt-5 mt-3'>Star Mount Technology</p>
+     <p className='text-white sm:text-sm text-[0.3rem]'>Plot No.304 Kh. No.-1074,Kapashera</p>
+     <p className='text-white sm:text-sm text-[0.3rem]'>New Delhi-110037 India </p>
+     <div className='flex gap-1 sm:gap-5 sm:mt-5 mt-3'>
          <div className='sm:h-7 sm:w-7 h-4 w-4 border rounded-full flex justify-center items-center'>
          <img src={facebookImg} className='sm:w-[20%] w-[15%] sm:h-[0.8rem] h-[0.5rem] '></img>
          </div>
@@ -35,7 +35,7 @@ const Footer = () => {
  </div>
 
  {/* footer part-2 */}
- <div className='flex flex-col sm:gap-4 gap-2 md:ml-14 sm:ml-5 -ml-10'>
+ <div className='flex flex-col sm:gap-4 gap-1 md:ml-14 sm:ml-5 -ml-5'>
      <p className='text-white sm:text-[1.25rem] text-[0.9rem]'>Our Services</p>
      <p className='text-white sm:text-xs text-[0.5rem]'>ERP</p>
      <p className='text-white sm:text-xs text-[0.5rem]'>CMS</p>
@@ -43,7 +43,7 @@ const Footer = () => {
  </div>
 
  {/* footer part-3 */}
- <div className='flex flex-col sm:gap-4 gap-2'>
+ <div className='flex flex-col sm:gap-4 gap-1 -ml-4'>
      <p className='text-white sm:text-[1.25rem] text-[0.9rem]'>About</p>
      <p className='text-white sm:text-xs text-[0.5rem]'>About Us</p>
      <p className='text-white sm:text-xs text-[0.5rem]'>Hiring</p>
